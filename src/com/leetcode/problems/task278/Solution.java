d46958209381ec55cd7a84cc67122bbd70e983ba
Emil <mr.emilka0@gmail.com>
2023-01-06 05:29:54 +0500

    Solved problem 278

--- src/com/leetcode/problems/task278/Solution.java
+++ src/com/leetcode/problems/task278/Solution.java
@@ -5,11 +5,8 @@ package com.leetcode.problems.task278;
 
 public class Solution extends VersionControl {
 
-    private int n;
-    private int bad;
 
     public int firstBadVersion(int n, int bad) {
-        this.bad = bad;
         int left = 0;
         int right = n;
         int mid = (left + right) / 2;
@@ -25,6 +22,6 @@ public class Solution extends VersionControl {
         if (isBadVersion(mid, bad))
             return mid;
         else
-            return mid+1;
+            return mid + 1;
     }
 }

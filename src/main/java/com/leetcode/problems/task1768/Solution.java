d21d1414d8f360ddf4b56a5f4d709cf2114ea207
Emil <mr.emilka0@gmail.com>
2023-06-01 06:03:37 +0600

    Solved task 1768 with test.

--- src/main/java/com/leetcode/problems/task1768/Solution.java
+++ src/main/java/com/leetcode/problems/task1768/Solution.java
@@ -3,10 +3,12 @@ package com.leetcode.problems.task1768;
 public class Solution {
     public String mergeAlternately(String word1, String word2) {
         StringBuilder result = new StringBuilder();
-        for (int i = 0, k = 0; i < word1.length() && k < word2.length(); i++, k++) {
-            result.append(word1.charAt(i));
-            result.append(word2.charAt(k));
+        for (int i = 0, k = 0, l = 0; i < word1.length() + word2.length(); i++, k++, l++) {
+            if (k < word1.length())
+                result.append(word1.charAt(k));
+            if (l < word2.length())
+                result.append(word2.charAt(l));
         }
-        return String.valueOf(result);
+        return result.toString();
     }
 }
--- src/test/java/com/leetcode/problems/task1768/SolutionTest.java
+++ src/test/java/com/leetcode/problems/task1768/SolutionTest.java
@@ -2,13 +2,25 @@ package com.leetcode.problems.task1768;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class SolutionTest {
 
     @Test
     void abcPlusPqrIsEqualApbqcr() {
         var solution = new Solution();
-        assertArrayEquals("apbqcr".toCharArray(), solution.mergeAlternately("abc","pqr").toCharArray());
+        assertArrayEquals("apbqcr".toCharArray(), solution.mergeAlternately("abc", "pqr").toCharArray());
+    }
+
+    @Test
+    void abPlusPqrsIsEqualApbqrs() {
+        var solution = new Solution();
+        assertArrayEquals("apbqrs".toCharArray(), solution.mergeAlternately("ab", "pqrs").toCharArray());
+    }
+
+    @Test
+    void abcdPlusPqIsEqualApbqcd() {
+        var solution = new Solution();
+        assertArrayEquals("apbqcd".toCharArray(), solution.mergeAlternately("abcd", "pq").toCharArray());
     }
 }
